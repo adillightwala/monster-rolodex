35f67c7d1f3c3c386d61542e141479ed15a67b33
unknown <adillightwala20@gmail.com>
2020-10-07 22:33:33 +0530

    Updates

---
+++ precache-manifest.a33909a153a86842b4f2b977b0b04be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b35cb201831b0af2275cae7500772f",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "250b68154fb2f810b66f",
+    "url": "/monster-rolodex/static/css/main.b2961110.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250b68154fb2f810b66f",
+    "url": "/monster-rolodex/static/js/main.5532703a.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
